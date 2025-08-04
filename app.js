acfad3f2b6eb7cd865c4efa4ca7f472d252e9770
AhmedRaza186 <sallumoon650@gmail.com>
2025-08-04 14:11:14 +0500

    JSassign2Complete

---
+++ app.js
@@ -0,0 +1,18 @@
+var username
+var myName ="Ahmed Raza"
+var message
+message ="Hello World"
+alert(message)
+var studentName ="Jameel"
+var studentAge ="16"
+var studentPassion ="Designer"
+alert(studentName)
+alert(studentAge)
+alert(studentPassion)
+var pizza ="PIZZA\nPIZZ\nPIZ\nPI\nP "
+alert(pizza)
+var email ="myemail@email.com"
+alert(`My email address is ${email}`)
+var book ="A smarter way to learn JavaScript"
+alert(`I am trying to learn from the Book ${book}`)
+document.writeln("Yah! I can write HTML content through JavaScript")
